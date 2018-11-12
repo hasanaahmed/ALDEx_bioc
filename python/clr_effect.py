d1756ba1d7e38dd5c0ef42f6686fc14553dd4187
Hasan Ahmed <hasanahmed@invitetech.com>
2018-11-12 04:42:06 -0500

    Update python/clr_effect.py

--- python/clr_effect.py
+++ python/clr_effect.py
@@ -3,19 +3,22 @@
 # returns the median effect size and proportion of effect that overlaps 0
 # data is returned in a data frame
 # requires multicore
+import pandas as pd
+import Rvect as rvect
+import numpy as np.r
 
-def aldex_effect(clr, verbose=True, include.sample.summary=False, useMC=False):
+def aldex_effect(clr, verbose=True, include_sample.summary=False, useMC=False):
 
   # Use clr conditions slot instead of input
     conditions = clr@conds
 
-    is.multicore = False
+    is_multicore = False
 
     bioP = "BiocParallel"
     if bioP in rownames(installed.packages()) & useMC:
         print("multicore environment is OK -- using the BiocParallel package")
         #require(BiocParallel)
-        is.multicore = True
+        is_multicore = True
 
     else:
         message("operating in serial mode")
@@ -34,7 +37,7 @@ def aldex_effect(clr, verbose=True, include.sample.summary=False, useMC=False):
     if length(levels) != 2:
         stop("only two condition levels are currently supported")
 
-    levels = vector("list", length(levels))
+    levels = rvect("list", length(levels))
     names(levels) = levels(conditions)
 
     for l in levels(conditions):
@@ -48,9 +51,9 @@ def aldex_effect(clr, verbose=True, include.sample.summary=False, useMC=False):
 
     # Summarize the relative abundance (rab) win and all groups
 
-    rab = vector( "list", 3 )
-    names(rab) = c( "all", "win", "spl" )
-    rab$win = list()
+    rab = rvect( "list", 3 )
+    names(rab) = np.r_( "all", "win", "spl" )
+    rab$win = tuple()
 
     #this is the median value across all monte carlo replicates
     cl2p = NULL
@@ -75,9 +78,9 @@ def aldex_effect(clr, verbose=True, include.sample.summary=False, useMC=False):
 
     if verbose == True:
         print("rab.win  complete")
-        if is.multicore == True:
+        if is_multicore == True:
             rab$spl = bplapply(getMonteCarloInstances(clr), function(m) {t(apply( m, 1, median))})
-        if (is.multicore == False):
+        if (is_multicore == False):
             rab$spl = lapply( getMonteCarloInstances(clr), function(m) {t(apply(m, 1, median))})
 
     if (verbose == True):
@@ -86,9 +89,9 @@ def aldex_effect(clr, verbose=True, include.sample.summary=False, useMC=False):
     # ---------------------------------------------------------------------
     # Compute diffs btw and win groups
 
-    l2d = vector("list", 2)
-    names(l2d) = c("btw", "win")
-    l2d$win = list()
+    l2d = rvect("list", 2)
+    names(l2d) = nm.r_("btw", "win")
+    l2d$win = tuple()
 
     # abs( win-conditions diff ), btw smps
 #this generates a linear sample of the values rather than an exhaustive sample
@@ -114,12 +117,12 @@ if verbose == True:
     print("within sample difference calculated")
     # Handle the case when the groups have different spl sizes
     # get the minimum number of win spl comparisons
-    ncol.wanted = min( sapply(l2d$win, ncol))
+    ncol_wanted = min( sapply(l2d$win, ncol))
 # apply multicore paradigm ML
-    if is.multicore == True:
-        l2d$win  = bplapply(l2d$win, function(arg) {arg[,1:ncol.wanted]})
-    if is.multicore == False:
-        l2d$win  <- lapply(l2d$win, function(arg) {arg[,1:ncol.wanted]})
+    if is_multicore == True:
+        l2d$win  = bplapply(l2d$win, function(arg) {arg[,1:ncol_wanted]})
+    if is_multicore == False:
+        l2d$win  <- lapply(l2d$win, function(arg) {arg[,1:ncol_wanted]})
 
     # btw condition diff (signed)
     #get the btw condition as a random sample rather than exhaustive search
@@ -130,11 +133,11 @@ if verbose == True:
     for l2 in levels[[2]]:
         concatl2 = cbind(getMonteCarloReplicate(clr,l2),concatl2)
 
-    sample.size = min(ncol(concatl1), ncol(concatl2))
+    sample_size = min(ncol(concatl1), ncol(concatl2))
 
-    if (sample.size < 10000):
-        smpl1 = t(apply(concatl1, 1, function(x){sample(x, sample.size)}))
-        smpl2 = t(apply(concatl2, 1, function(x){sample(x, sample.size)}))
+    if (sample_size < 10000):
+        smpl1 = t(apply(concatl1, 1, function(x){sample(x, sample_size)}))
+        smpl2 = t(apply(concatl2, 1, function(x){sample(x, sample_size)}))
     else:
         smpl1 = t(apply(concatl1, 1, function(x){sample(x, 10000)}))
         smpl2 = t(apply(concatl2, 1, function(x){sample(x, 10000)}))
@@ -148,33 +151,35 @@ if verbose == True:
     if verbose == True:
         print("between group difference calculated")
 
-    win.max = matrix( 0 , nrow=nr , ncol=ncol.wanted )
+    max(win) = matrix( 0 , nrow=nr , ncol=ncol_wanted )
     l2d$effect = matrix( 0 , nrow=nr , ncol=ncol(l2d$btw) )
     rownames(l2d$effect) = rn
 
 ###the number of elements in l2d$btw and l2d$win may leave a remainder when
-  #recycling these random vectors. Warnings are suppressed because this is not an issue
+  #recycling these random rvects. Warnings are suppressed because this is not an issue
   #for this calculation. In fact, any attempt to get rid of this error would
-  #decrease our power as one or both vectors would need to be truncated gg 20/06/2013
+  #decrease our power as one or both rvects would need to be truncated gg 20/06/2013
 
     options(warn=-1)
 
     for i in 1:nr:
-        win.max[i,] = apply( ( rbind( l2d$win[[1]][i,] , l2d$win[[2]][i,] ) ) , 2 , max )
-        l2d$effect[i,] = l2d$btw[i,] / win.max[i,]
+        first = l2d$win[[1]][i,]
+        second = l2d$win[[2]][i,]
+        max(win([i,])) = apply(first.append(second))
+        l2d$effect[i,] = l2d$btw[i,] / win_max[i,]
 
     options(warn=0)
 
-    rownames(win.max) = rn
-    attr(l2d$win,"max") = win.max
-    rm(win.max)
+    rownames(max(win)) = rn
+    attr(l2d$win,"max") = max(win)
+    rm(max(win))
 
     # ---------------------------------------------------------------------
     # Summarize diffs
 
-    l2s = vector( "list", 2 )
-    names( l2s ) = c( "btw", "win" )
-    l2s$win = list()
+    l2s = rvect( "list", 2 )
+    names( l2s ) = np.r_( "btw", "win" )
+    l2s$win = tuple()
 
     l2s$btw = t(apply( l2d$btw, 1, median ))
     l2s$win = t(apply( attr(l2d$win,"max"), 1, median ))
@@ -190,7 +195,7 @@ if verbose == True:
 
 # make and fill in the data table
 # i know this is inefficient, but it works and is not a bottleneck
-    rv = list(
+    rv = tuple(
         rab = rab,
         diff = l2s,
         effect = effect,
@@ -200,23 +205,23 @@ if verbose == True:
 if verbose == True:
     print("summarizing output")
 
-   y.rv = data.frame(t(rv$rab$all))
-   colnames(y.rv) = c("rab.all")
+   y_rv = data.frame(t(rv$rab$all))
+   colnames(y_rv) = cnp.r_("rab.all")
 
    for i in names(rv$rab$win):
-       nm = paste("rab.win", i, sep=".")
-       y.rv[,nm] = data.frame(t(rv$rab$win[[i]]))
+       nm = paste("rab_win", i, sep=".")
+       y_rv[,nm] = data.frame(t(rv$rab$win[[i]]))
 
-   if include.sample.summary == True:
+   if include_sample_summary == True:
       for(i in names(rv$rab$spl)){
          nm <- paste("rab.sample", i, sep=".")
-         y.rv[,nm] <- data.frame(t(rv$rab$spl[[i]]))
+         y_rv[,nm] <- data_frame(t(rv$rab$spl[[i]]))
 
    for i in names(rv$diff):
        nm = paste("diff", i, sep=".")
-       y.rv[,nm] = data.frame(t(rv$diff[[i]]))
+       y_rv[,nm] = data_frame(t(rv$diff[[i]]))
 
-   y.rv[,"effect"] = data.frame(t(rv$effect))
-   y.rv[,"overlap"] = data.frame(rv$overlap)
+   y_rv[,"effect"] = data_frame(t(rv$effect))
+   y_rv[,"overlap"] = data_frame(rv$overlap)
 
-   return(y.rv)
+   return(y_rv)
